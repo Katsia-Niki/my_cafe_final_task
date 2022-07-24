6f407d7a63008aa8eceaf5d2f076ab9e6ccc29b4
Katsiaryna Nikifarava <katsiaryna.niki@gmail.com>
2022-07-24 16:15:23 +0300

    load project

--- src/test/java/test/jwd/cafe/validator/impl/UserValidatorImplTest.java
+++ src/test/java/test/jwd/cafe/validator/impl/UserValidatorImplTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class UserValidatorImplTest {
-    private final UserValidator validator = UserValidatorImpl.getInstance();
+    UserValidator validator = UserValidatorImpl.getInstance();
 
     @DataProvider(name = "emailProvider")
     public Object[][] createData() {
